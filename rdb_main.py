2f963815bc2fe33f643a4f5098015578bb2f7c45
tkiley1 <Thomas_Kiley1@student.uml.edu>
2018-09-20 12:26:44 -0400

    updated math functions and fixed a few bugs... approx 8.5% success rate over 1,000,000 repititions

--- rdb_main.py
+++ rdb_main.py
@@ -21,59 +21,79 @@ def rtb():
     it = 0
     wins = 0
     games = 0
-    while((len(deck)/4) > 0):
+    while(len(deck) >= 4):
         guess = get_guess(it, seen_cards, curr_flip, deck)
+        #print (guess)
         card, deck = flip(deck)
+        #print(seen_cards)
+        #print(deck)
+        seen_cards = seen_cards + [card]
         if guess != get_color(card):
             curr_flip = []
             it = 0
             games = games + 1
-            print("Fucked up the color")
+         #   print("Fucked up the color")
             continue
-        seen_cards = seen_cards + [card]
         curr_flip = curr_flip + [card]
+        #print(seen_cards)
+        #print(deck)
         it = it + 1
         guess = get_guess(it, seen_cards, curr_flip, deck)
+        #print(guess)
         card, deck = flip(deck)
-        if guess == "Higher" and get_value(card) <= curr_flip[0] or (guess == "Lower" and get_value(card) >= curr_flip[0]):
+        seen_cards = seen_cards + [card]
+        if (guess == "Higher" and get_value(card) <= get_value(curr_flip[0])) or (guess == "Lower" and get_value(card) >= get_value(curr_flip[0])):
             curr_flip = []
             it = 0
             games = games + 1
-            print("Fucked up higher or lower")
+         #   print("Fucked up higher or lower")
             continue
-        seen_cards = seen_cards + [card]
         curr_flip = curr_flip + [card]
+        #print(seen_cards)
+        #print(deck)
         tmp = []
         for i in curr_flip:
             tmp = tmp + [get_value(i)]
         tmp = sorted(tmp)
         it = it + 1
         guess = get_guess(it, seen_cards, curr_flip, deck)
+        #print(guess)
         card, deck = flip(deck)
-        if (guess == "Between" and (get_value(card) >= tmp[0] or get_value(card) <= tmp[1])) or  (guess == "Outside" and (get_value(card) <= tmp[0] or get_value(card) >= tmp[1])):
+        seen_cards = seen_cards + [card]
+        if ((guess == "Between" and (get_value(card) > tmp[0] and get_value(card) < tmp[1])) or  (guess == "Outside" and (get_value(card) < tmp[0] or get_value(card) > tmp[1]))):
+            pass
+        else:
             curr_flip = []
             it = 0
             games = games + 1
-            print("Fucked up between or outside")
+            #print("Fucked up between or outside")
             continue
-        seen_cards = seen_cards + [card]
         curr_flip = curr_flip + [card]
+        #print(seen_cards)
+        #print(deck)
         it = it + 1
         guess = get_guess(it, seen_cards, curr_flip, deck)
+        #print (guess)
         card, deck = flip(deck)
+        seen_cards = seen_cards + [card]
         if guess != get_suit(card):
             curr_flip = []
             it = 0
             games = games + 1
-            print("Fucked up the suit")
+            #print("Fucked up the suit")
             continue
         else:
             wins = wins + 1
             games = games + 1
             curr_flip = []
             it = 0
-            print("Didn't fuck up")
+            #print("Didn't fuck up")
             continue
-    return wins, games#, float(wins/games)
-
-print(rtb())
+    return wins, games, 100*(float(wins)/games)
+ovr_pct = 0
+for i in range(1000000):
+    wins, games, pct = rtb()
+    ovr_pct = pct + ovr_pct
+    print i
+ovr_pct = ovr_pct / 1000000
+print ovr_pct
--- rtb.py
+++ rtb.py
@@ -13,31 +13,37 @@ def get_value(card):
 
 def get_guess(it, seen_cards, curr_flip, deck):
     if it == 0:
+        red = 0
+        black = 0
         seen_color = 0
-        for i in range(len(seen_cards)):
-            seen_color = get_color(i) + seen_color
-        try:
-            avg_color_seen = float(seen_color/len(seen_cards))
-        except:
-            avg_color_seen = 0.5
-        if avg_color_seen < 0.5:
+        for i in seen_cards:
+            if get_color(i) == 1:
+                red = red + 1
+            else:
+                black = black + 1
+        if red < black:
             return 1
-        if avg_color_seen > 0.5:
+        if red > black:
             return 0
-        if avg_color_seen == 0.5:
+        else:
             return random.randint(0,1)
     if it == 1:
         seen_value = 0
-        for i in range(len(seen_cards)):
-            seen_value = get_value(i) + seen_value
-        avg_value_seen = float(seen_value/len(seen_cards))
-        if avg_value_seen < get_value(curr_flip[0]):
+        high = 0
+        low = 0
+        for i in seen_cards:
+            if get_value(i) > get_value(curr_flip[0]):
+                high = high + 1
+            if get_value(i) < get_value(curr_flip[0]):
+                low = low + 1
+        if high > low:
             return "Higher"
-        if avg_value_seen > get_value(curr_flip[0]):
+        if low > high:
             return "Lower"
-        if avg_value_seen == get_value(curr_flip[0]):
+        if high == low:
             options = ["Higher","Lower"]
             return random.choice(options)
+    
     if it == 2:
         lowc = get_value(curr_flip[0])
         highc = get_value(curr_flip[1])
@@ -45,16 +51,16 @@ def get_guess(it, seen_cards, curr_flip, deck):
         cards_seen_outside = 0
         cards_seen_inside = 0
 
-        for i in range(len(seen_cards)):
+        for i in seen_cards:
             if get_value(i) > highc or get_value(i) < lowc:
                 cards_seen_outside = cards_seen_outside + 1
             if get_value(i) < highc and get_value(i) > lowc:
                 cards_seen_inside = cards_seen_inside + 1
         
         if cards_seen_outside > cards_seen_inside:
-            return("Between")
-        if cards_seen_outside < cards_seen_inside:
             return("Outside")
+        if cards_seen_outside < cards_seen_inside:
+            return("Between")
         if cards_seen_outside == cards_seen_inside:
             opt_bo = ["Between","Outside"]
             return random.choice(opt_bo)
@@ -64,7 +70,7 @@ def get_guess(it, seen_cards, curr_flip, deck):
         c = 0
         s = 0
 
-        for i in range(len(seen_cards)):
+        for i in seen_cards:
             if get_suit(i) == 0:
                 h = h + 1
             if get_suit(i) == 1:
